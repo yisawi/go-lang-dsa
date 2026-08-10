d6b700ebee9591b01efd497bf76736e9b4551ffe
Yusuf <contact@yisawi.me>
2026-08-10 04:22:46 +0300

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.go
@@ -0,0 +1,11 @@
+func hasDuplicate(nums []int) bool {
+    seen := make(map[int]bool)
+	for i := 0; i < len(nums); i++ {
+		if seen[nums[i]] {
+			return true
+		} else {
+			seen[nums[i]] = true
+		}
+	}
+	return false
+	}
